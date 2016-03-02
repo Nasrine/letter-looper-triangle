b730d0888f35a5f0371969cbad0a4a118830cea8
Nasrine <azar0006@algonquinlive.com>
2016-03-02 12:15:06 -0500

    added semicolon on line 8

--- js/main.js
+++ js/main.js
@@ -1,19 +1,13 @@
 var newName = prompt('Enter a letter');
-// to write comments//
 
 var i;
 var j;
 
 for (i = 0; i < 10; i++) {
   for (j = 0; j < i; j++) {
-  document.write(newName );
+    document.write(newName);
 }
-document.write ('<br>')
+document.write ('<br>');
 }
-
-/*var add = b + b;
-  number += bb;*/
-
-
-
-
+/* the 'i is in range between 0 to 10
+for the 'j' range would be 0 to 'i'*/

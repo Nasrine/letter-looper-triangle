86e0b202bf8ad287bcb002622ace2ba3d1d5e7ca
Nasrine <azar0006@algonquinlive.com>
2016-03-02 12:00:29 -0500

    loop inside another loop

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var newName = prompt('Enter a letter');
+// to write comments//
+
+var i;
+var j;
+
+for (i = 0; i < 10; i++) {
+  for (j = 0; j < i; j++) {
+  document.write(newName );
+}
+document.write ('<br>')
+}
+
+/*var add = b + b;
+  number += bb;*/
+
+
+
+
